aa5fd4bbe5a44f642d7f6cd84c41748f336af55b
StefanHancar <stefanhancar@MacBook-Pro-uzivatela-StefanHancar.local>
2023-05-07 12:35:38 +0200

    initial commit

---
+++ components/UI/Testimonial.jsx
@@ -0,0 +1,141 @@
+import React from "react"
+import { Container, Row, Col } from "reactstrap"
+import Image from "next/image"
+import SectionSubtitle from "./SectionSubtitle"
+import network from "../../public/images/Connected world.png"
+import Slider from "react-slick"
+import classes from "../../styles/testimonial.module.css"
+
+const Testimonial = () => {
+  const settings = {
+    dots: false,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    infinite: true,
+    swipeToSlide: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="6" md="6" className={`${classes.testimonial__img}`}>
+            <Image alt="network-img" src={network} width="400" height="400" />
+          </Col>
+
+          <Col lg="6" md="6">
+            <SectionSubtitle subtitle="Experience" />
+            <h4 className="mt-4 mb-5">Working and volunteering experiences</h4>
+
+            <Slider {...settings}>
+              <div className={`${classes.testimonial__item}`}>
+                <div className={`${classes.testimonial__client}`}>
+                  <Image
+                    alt="client-img"
+                    src="/images/hero.jpg"
+                    width="50"
+                    height="50"
+                    className=" rounded-2"
+                  />
+
+                  <div>
+                    <h6>IBM</h6>
+                    <h6>Frontend developer</h6>
+                  </div>
+                </div>
+
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi saepe eveniet dolores molestias nisi ullam quis
+                  delectus inventore, quidem beatae ipsa pariatur doloribus
+                  corrupti! Quisquam expedita minima, neque et quis, fugiat hic
+                  iste possimus vitae perspiciatis consequatur quod modi
+                  dignissimos.
+                </p>
+              </div>
+
+              <div className={`${classes.testimonial__item}`}>
+                <div className={`${classes.testimonial__client}`}>
+                  <Image
+                    alt="client-img"
+                    src="/images/hero.jpg"
+                    width="50"
+                    height="50"
+                    className=" rounded-2"
+                  />
+
+                  <div>
+                    <h6>Freelance</h6>
+                    <h6>Web Developer</h6>
+                  </div>
+                </div>
+
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi saepe eveniet dolores molestias nisi ullam quis
+                  delectus inventore, quidem beatae ipsa pariatur doloribus
+                  corrupti! Quisquam expedita minima, neque et quis, fugiat hic
+                  iste possimus vitae perspiciatis consequatur quod modi
+                  dignissimos.
+                </p>
+              </div>
+
+              <div className={`${classes.testimonial__item}`}>
+                <div className={`${classes.testimonial__client}`}>
+                  <Image
+                    alt="client-img"
+                    src="/images/hero.jpg"
+                    width="50"
+                    height="50"
+                    className=" rounded-2"
+                  />
+
+                  <div>
+                    <h6>Work and Travel</h6>
+                    <h6>Student worker</h6>
+                  </div>
+                </div>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi saepe eveniet dolores molestias nisi ullam quis
+                  delectus inventore, quidem beatae ipsa pariatur doloribus
+                  corrupti! Quisquam expedita minima, neque et quis, fugiat hic
+                  iste possimus vitae perspiciatis consequatur quod modi
+                  dignissimos.
+                </p>
+              </div>
+              <div className={`${classes.testimonial__item}`}>
+                <div className={`${classes.testimonial__client}`}>
+                  <Image
+                    alt="client-img"
+                    src="/images/hero.jpg"
+                    width="50"
+                    height="50"
+                    className=" rounded-2"
+                  />
+
+                  <div>
+                    <h6>Volunteering</h6>
+                    <h6>Student worker</h6>
+                  </div>
+                </div>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi saepe eveniet dolores molestias nisi ullam quis
+                  delectus inventore, quidem beatae ipsa pariatur doloribus
+                  corrupti! Quisquam expedita minima, neque et quis, fugiat hic
+                  iste possimus vitae perspiciatis consequatur quod modi
+                  dignissimos.
+                </p>
+              </div>
+            </Slider>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
+
+export default Testimonial
---
+++ components/data/portfolio.js
@@ -0,0 +1,48 @@
+const portfolio = [
+  {
+    id: "01",
+    title: "Gatsby coffe shop",
+    img: "/images/portfolio-01.jpg",
+    category: "Mobile App",
+    keyword: ["Web app"],
+    liveUrl: "https://eshopkava.netlify.app/",
+  },
+
+  {
+    id: "02",
+    title: "Super store",
+    img: "/images/portfolio-02.jpg",
+    category: "Mobile App",
+    keyword: ["App", "ForFun"],
+    liveUrl: "https://hancar-eshop.netlify.app/",
+  },
+
+  {
+    id: "03",
+    title: "To be updated",
+    img: "/images/portfolio-03.jpg",
+    category: "Mobile App",
+    keyword: ["Mobile", "App", "UI-UX"],
+    liveUrl: "#",
+  },
+
+  {
+    id: "04",
+    title: "To be updated",
+    img: "/images/portfolio-04.jpg",
+    category: "Mobile App",
+    keyword: ["Mobile", "App", "UI-UX"],
+    liveUrl: "#",
+  },
+
+  {
+    id: "05",
+    title: "To be updated",
+    img: "/images/portfolio-05.jpg",
+    category: "Mobile App",
+    keyword: ["Mobile", "App", "UI-UX"],
+    liveUrl: "#",
+  },
+]
+
+export default portfolio
