ce05675043e2f5b04e607249882d2aa27522abab
Stefan Hancar <stefan@hancar.sk>
2024-02-24 14:58:30 +0100

    fix issue

--- components/UI/Blog.jsx
+++ components/UI/Blog.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "../../styles/about.module.css";
 import Link from "next/link";
 const Blog = () => {
-  return <a href="https://sensational-travesseiro-b924f3.netlify.app/">Blog</a>;
+  return <div></div>;
 };
 
 export default Blog;
